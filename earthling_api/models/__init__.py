5d2c491034200a0cdb70d7a9b27bfeb03adf659a
Maria Sanli <marisanli.93@gmail.com>
2024-08-23 17:28:06 -0400

    models created

---
+++ earthling_api/models/__init__.py
@@ -0,0 +1,5 @@
+from .subject import Subject
+from .entry import Entry
+from .tag import Tag
+from .entry_tag import EntryTag
+from .user import User
---
+++ earthling_api/models/subject.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Subject(models.Model): 
+    age = models.IntegerField()
+    native_language = models.CharField(max_length=100)
+    other_languages = models.TextField()
+    additional_info = models.TextField(blank=True)
