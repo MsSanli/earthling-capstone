0c4a447d5498cfec0b6b682de296bf0353690e1d
Maria Sanli <marisanli.93@gmail.com>
2024-08-28 21:00:23 -0400

    create and update for subject complete

--- earthling_api/views/subject_view.py
+++ earthling_api/views/subject_view.py
@@ -21,6 +21,21 @@ class SubjectView(ViewSet):
         subject = get_object_or_404(Subject, pk=pk)
         subject.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+      
+    def create(self, request):
+        serializer = SubjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk):
+        subject = get_object_or_404(Subject, pk=pk)
+        serializer = SubjectSerializer(subject, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
 
 
 class SubjectSerializer(serializers.ModelSerializer):
