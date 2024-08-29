9b2eda644dd68121206b99cb560ba4d2ec3cf5f9
Maria Sanli <marisanli.93@gmail.com>
2024-08-28 21:52:12 -0400

    language create and update

--- earthling_api/views/language_view.py
+++ earthling_api/views/language_view.py
@@ -4,22 +4,76 @@ from rest_framework import status, serializers
 from django.shortcuts import get_object_or_404
 from earthling_api.models import Language
 
-class LanguageView(ViewSet):  # type: ignore
-  def retrieve(self, request, pk):
-      language = get_object_or_404(Language, pk=pk)
-      serializer = LanguageSerializer(language)
-      return Response(serializer.data)
+class LanguageView(ViewSet):  
+    def retrieve(self, request, pk):
+        language = get_object_or_404(Language, pk=pk)
+        serializer = LanguageSerializer(language)
+        return Response(serializer.data)
     
-#all languages
-  def list(self, request):
-      languages = Language.objects.all()
-      serializer = LanguageSerializer(languages, many=True)
-      return Response(serializer.data)
+    def list(self, request):
+        languages = Language.objects.all()
+        serializer = LanguageSerializer(languages, many=True)
+        return Response(serializer.data)
 
-  def destroy(self, request, pk):
-      language = get_object_or_404(Language, pk=pk)
-      language.delete()
-      return Response(status=status.HTTP_204_NO_CONTENT)
+    def create(self, request):
+        serializer = LanguageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk):
+        language = get_object_or_404(Language, pk=pk)
+        serializer = LanguageSerializer(language, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk):
+        language = get_object_or_404(Language, pk=pk)
+        language.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ['id', 'name', 'region', 'number_of_speakers', 'pronunciation', 'language_family', 'writing_system']
+from rest_framework import viewsets, status, serializers
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from earthling_api.models import Language
+
+class LanguageViewSet(viewsets.ViewSet):
+    def list(self, request):
+        languages = Language.objects.all()
+        serializer = LanguageSerializer(languages, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = LanguageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        language = get_object_or_404(Language, pk=pk)
+        serializer = LanguageSerializer(language)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        language = get_object_or_404(Language, pk=pk)
+        serializer = LanguageSerializer(language, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        language = get_object_or_404(Language, pk=pk)
+        language.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
