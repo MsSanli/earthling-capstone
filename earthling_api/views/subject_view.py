1b3a113ac739e646ebe3a1d4a18a5401da60569b
Maria Sanli <marisanli.93@gmail.com>
2024-09-05 21:44:22 -0400

    subject tests working

--- earthling_api/views/subject_view.py
+++ earthling_api/views/subject_view.py
@@ -29,13 +29,17 @@ class SubjectView(ViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def update(self, request, pk):
+    def update(self, request, pk=None):
         subject = get_object_or_404(Subject, pk=pk)
         serializer = SubjectSerializer(subject, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        return self.update(request, pk)
+    
 
 
 class SubjectSerializer(serializers.ModelSerializer):
