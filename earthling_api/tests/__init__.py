1f8369475c590d203cecbb5c570517849b22c2d7
Maria Sanli <marisanli.93@gmail.com>
2024-09-05 20:23:45 -0400

    language tests passed

--- earthling_api/tests/__init__.py
+++ earthling_api/tests/__init__.py
@@ -1,1 +1,3 @@
 from .user_tests import UserTests
+from .entry_tests import EntryTests
+from .language_tests import LanguageTests
--- earthling_api/views/language_view.py
+++ earthling_api/views/language_view.py
@@ -29,48 +29,11 @@ class LanguageView(ViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def partial_update(self, request, pk=None):
+        return self.update(request, pk)
 
     def destroy(self, request, pk):
-        language = get_object_or_404(Language, pk=pk)
-        language.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class LanguageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Language
-        fields = ['id', 'name', 'region', 'number_of_speakers', 'pronunciation', 'language_family', 'writing_system']
-from rest_framework import viewsets, status, serializers
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from earthling_api.models import Language
-
-class LanguageViewSet(viewsets.ViewSet):
-    def list(self, request):
-        languages = Language.objects.all()
-        serializer = LanguageSerializer(languages, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = LanguageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        language = get_object_or_404(Language, pk=pk)
-        serializer = LanguageSerializer(language)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        language = get_object_or_404(Language, pk=pk)
-        serializer = LanguageSerializer(language, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
         language = get_object_or_404(Language, pk=pk)
         language.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
